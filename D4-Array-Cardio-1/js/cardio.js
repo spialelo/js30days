e81cd76b34e7f60619e8a5a4877bc0b5738837f8
spialelo <ndphillips@gmail.com>
2017-01-15 13:58:24 -0500

    Starte Flex panels in JavaScript30 challenge

--- D4-Array-Cardio-1/js/cardio.js
+++ D4-Array-Cardio-1/js/cardio.js
@@ -18,12 +18,7 @@ const inventors = [{ first: 'Albert', last: 'Einstein', year: 1879, passed: 1955
 /*An array of strings*/
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
-
-/*Another array of strings*/
-const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
-
-
-// Array.prototype.filter()
+//Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
 console.table(fifteen);
@@ -57,10 +52,39 @@ console.table(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-const category = document.querySelector('.mw-category');
+//const category = document.querySelector('.mw-category');
+//const links = Array.from(category.querySelectorAll('a'));
+
+/* Return the textContent of each 'a' into a new array */
+//const de = links
+//.map(link => link.textContent)
+//.filter(streetName => streetName.includes('de'));
+
+/*Test the above code in console on wikipedia page*/
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+    const [aLast, aFirst] = lastOne.split(', ');
+    const [bLast, bFirst] = nextOne.split(', ');
+    return aLast > bLast ? 1 : -1;
+
+});
+console.log(alpha);
+
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
+
+/*Another array of strings*/
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+const transportation = data.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 0;
+    }
+    obj[item]++;
+    return obj;
+}, {});
+
+console.log(transportation);
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 JavaScript 30 days challenge
 Starter files retrieved from: https://github.com/wesbos/JavaScript30
 **2017.01.13** - JavaScript Drumkit completed. Understanding ES6 quite well so far. Half-finished the JS Clock.
-**2017.01.14** - Completed JS Clock and CSs Variable w/ JS challenge.
-**2017.01.15** - 
+**2017.01.14** - Completed JS Clock and CSS Variable w/ JS challenge.
+**2017.01.15** - Completed Array Cardio Day 1 and Flex Panels Image Gallery.
