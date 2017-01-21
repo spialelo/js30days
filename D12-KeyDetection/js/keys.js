d352587b688c8878c44ed79756082705fc83fb85
spialelo <ndphillips@gmail.com>
2017-01-21 14:49:53 -0500

    Cornify is kind of cute.

--- D12-KeyDetection/js/keys.js
+++ D12-KeyDetection/js/keys.js
@@ -4,6 +4,13 @@ const secretCode = 'wesbos';
 window.addEventListener('keyup', (e) => {
     console.log(e.key);
     pressed.push(e.key);
+
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+    if (pressed.join('').includes(secretCode)) {
+        console.log('Gong! Confetti! It is included.');
+        cornify_add();
+    }
     console.log(pressed);
 
 });
