0ffdff6b6e3bb3e9a6235f6170f2a6ecd346e242
spialelo <ndphillips@gmail.com>
2017-01-27 21:57:46 -0500

    Completed the sort bands

--- D17-SortBand/js/sortband.js
+++ D17-SortBand/js/sortband.js
@@ -1,1 +1,15 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+//get the ul with id bands, then use .map to loop through the sorted bands and push them into the ul. Throw a join at the end of the .map to turn the array into a string.
+document.querySelector('#bands').innerHTML =
+    sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
+
+console.log(sortedBands);
