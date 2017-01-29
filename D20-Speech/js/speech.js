58a152e6a5aee805ce753dc91adb114fdf064506
spialelo <ndphillips@gmail.com>
2017-01-28 22:50:56 -0500

    Work on day 20 again with server. For now it's been done

--- D20-Speech/js/speech.js
+++ D20-Speech/js/speech.js
@@ -8,7 +8,22 @@
  words.appendChild(p);
 
  recognition.addEventListener('results', e => {
-     console.log(e);
+
+     const transcript = Array.from(e.results)
+         .map(results => result[0])
+         .map(result => result.transcript)
+         .join('');
+
+     p.textContent = transcript;
+
+     if (e.results[0].isFinal) {
+         p = document.createElement('p');
+         words.appendChild(p);
+     }
+
  });
+
+
+ recognition.addEventListener('end', recognition.start);
 
  recognition.start();
