ef2d0251b70d66c7dd0afb67383b6471d38762ee
spialelo <ndphillips@gmail.com>
2017-02-08 23:52:44 -0500

    Day 25 event-capture-once completed

--- D25-Event-Capture/js/event.js
+++ D25-Event-Capture/js/event.js
@@ -1,8 +1,16 @@
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
 
 function logText(e) {
     console.log(this.classList.value);
+    e.stopPropagation(); //stop bubbling
 }
 
 /* Knew this off the top of my head. JS30 days has stuck and it was a good idea to take a day or two off. */
-divs.forEach(div => div.addEventListener('click', logText));
+divs.forEach(div => div.addEventListener('click', logText, { capture: false }));
+
+/* Using once, fires all the events but only once when the button is clicked*/
+button.addEventListener('click', () => {
+    console.log('You clicked!!!');
+}, { once: true });
