1ab1e75aaeaefad2211d7f2c2ea9ec8a4fb7c05e
spialelo <ndphillips@gmail.com>
2017-02-13 22:41:22 -0500

    Progress on day 29

--- D29-Countdown/js/countdown.js
+++ D29-Countdown/js/countdown.js
@@ -1,10 +1,21 @@
+let countdown;
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
 
-    setInterval(() => {
-        const secondsLeft = then - Date.now();
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+        }
+        console.log(secondsLeft);
     }, 1000);
 
 
+}
+
+function displayTimeLeft(seconds) {
+    console.log(seconds);
 }
