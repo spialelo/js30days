c76b0751747c4f9a814b1ed22c02f670f49d2fc1
spialelo <ndphillips@gmail.com>
2017-02-13 23:31:25 -0500

    Ok. Now I'm definitely stopping and saving rest for tomorrow

--- D29-Countdown/js/countdown.js
+++ D29-Countdown/js/countdown.js
@@ -1,7 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
-// const minutes = document.querySelector('input').value;
-
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 // let seconds = minutes * 60;
 
 
@@ -9,6 +9,7 @@ function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -20,8 +21,8 @@ function timer(seconds) {
         //display it
         displayTimeLeft(secondsLeft);
     }, 1000);
-
 }
+
 
 function displayTimeLeft(seconds) {
     const minute = Math.floor(seconds / 60);
@@ -29,9 +30,20 @@ function displayTimeLeft(seconds) {
     const display = `${minute}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
     document.title = display;
     timerDisplay.textContent = display;
-
 }
+
 
 function displayEndTime(timestamp) {
     const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : '' }${minutes}`;
+    /*For non-24 hour: ${hour > 12 ? hour - 12 : hour} */
 }
+
+
+function startTime() {
+    console.log(this);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTime));
