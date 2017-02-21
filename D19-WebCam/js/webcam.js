15a71e8bc37a63bd4c83c26f5ba185ce603de710
spialelo <ndphillips@gmail.com>
2017-02-20 23:12:45 -0500

    Progress on day 19. This one is fun

--- D19-WebCam/js/webcam.js
+++ D19-WebCam/js/webcam.js
@@ -3,3 +3,24 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.error('Please allow the web cam, pretty please.', err);
+        });
+}
+
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+}
+
+
+getVideo();
